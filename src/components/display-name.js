a3510ac935c477d2257664a62da8b129c968b0c0
Duzie Uche-Abba <duzzi4x@gmail.com>
2022-07-06 02:36:37 +0200

    fourth commit

--- src/components/display-name.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DisplayName = () => {
-  return (
-    <div className='display-name'></div>
-  )
-}
-
-export default DisplayName
--- src/components/namewrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NameWrap = () => {
-  return (
-    <div className='name-wrap'></div>
-  )
-}
-
-export default NameWrap
